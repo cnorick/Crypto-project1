1d43171a83d95d3bcd2913cf407e65756c465ec8
cnorick <cnathanorick@gmail.com>
2017-09-21 21:14:18 -0400

    Implement cbc encrypt

--- src/cbc.py
+++ src/cbc.py
@@ -1,8 +1,18 @@
+import binascii
+from Crypto.Cipher import AES
+from helpers import pad
 
 def encrypt(message, key):
-    
-
-    return
+    if (message is None) or (len(message) == 0):
+        raise ValueError('message cannot be null or empty')
+    paddedMessage = pad(message, 16)
+    cipher = AES.AESCipher(key[:32], AES.MODE_ECB)
+    ciphertext = cipher.encrypt(paddedMessage)
+    return binascii.hexlify(bytearray(ciphertext)).decode('utf-8') 
 
 def decrypt(cipherText, key):
     return
+
+m = bytes('abcd', 'utf8')
+cipherText = encrypt(m, 'abcdefghijklmnopqrstuvwxyz123456')
+print(cipherText)
--- src/helpers.py
+++ src/helpers.py
@@ -5,32 +5,32 @@ message is a byte array.
 def pad(message, blockSize):
     if (message is None) or (len(message) == 0):
         raise ValueError('message cannot be null or empty')
-    if type(message) is not bytearray:
-        raise ValueError('message must be a bytearray')
-
+    if type(message) is not bytes:
+        raise ValueError('message must be bytes')
+    message = bytearray(message)
     paddingNeeded = blockSize - (len(message) % blockSize)
 
     paddedMessage = message[:]
     for i in range(paddingNeeded):
         paddedMessage.append(paddingNeeded)
 
-    return paddedMessage
+    return bytes(paddedMessage)
 
 '''
 Unpads a message padded by calling pad().
 message must be a byte array.
 '''
 def unpad(message):
-    if type(message) is not bytearray:
-        raise ValueError('message must be a bytearray')
-    
+    if type(message) is not bytes:
+        raise ValueError('message must be a bytes')
+    message = bytearray(message)
     numPadding = message[-1]
-    return message[:-numPadding]
+    return bytes(message[:-numPadding])
 
 '''
 Test that unpad correctly reverses pad.
 '''
 def paddingTest():
-    m = bytearray('abcde', 'utf8')
+    m = bytes('abcde', 'utf8')
     for i in range(1, 11):
         assert m == (unpad(pad(m, i)))
