7b15fc154f44e0d9b49711bbaccde8c65c6bf240
cnorick <cnathanorick@gmail.com>
2017-09-21 20:44:11 -0400

    Add unpad

--- src/helpers.py
+++ src/helpers.py
@@ -1,13 +1,12 @@
-import binascii
-from Crypto.Cipher import AES
-
 '''
 pads message to be a multiple of blocksize.
 message is a byte array.
 '''
 def pad(message, blockSize):
     if (message is None) or (len(message) == 0):
-        raise ValueError('input text cannot be null or empty set')
+        raise ValueError('message cannot be null or empty')
+    if type(message) is not bytearray:
+        raise ValueError('message must be a bytearray')
 
     paddingNeeded = blockSize - (len(message) % blockSize)
 
@@ -17,8 +16,21 @@ def pad(message, blockSize):
 
     return paddedMessage
 
+'''
+Unpads a message padded by calling pad().
+message must be a byte array.
+'''
 def unpad(message):
-    return
+    if type(message) is not bytearray:
+        raise ValueError('message must be a bytearray')
+    
+    numPadding = message[-1]
+    return message[:-numPadding]
 
-m = bytearray('abcd', 'utf8')
-print(pad(m, 4))
+'''
+Test that unpad correctly reverses pad.
+'''
+def paddingTest():
+    m = bytearray('abcde', 'utf8')
+    for i in range(1, 11):
+        assert m == (unpad(pad(m, i)))
